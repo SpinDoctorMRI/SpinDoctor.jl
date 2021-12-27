7e8c7e3a479461c1c65ea7e6a6cbf9470d40203c
Syver Døving Agdestein <syverda@icloud.com>
2021-12-27 13:59:17 +0100

    Add half and normal functionality

--- src/utils/unitcircle.jl
+++ src/utils/unitcircle.jl
@@ -7,10 +7,18 @@ If `half` is `true`, the directions lie on a half-circle instead of the whole ci
 """
 function unitcircle(n; half = false, normal = [0, 0, 1])
     # Create directions (unformly distributed on unit circle)
-    θ = 2π * collect(0:n-1)' / n
+    θ = 2π * collect(0:n-1)' / (1 + half)n
     directions = [
         cos.(θ)
         sin.(θ)
         zeros(1, ndirection)
     ]
+
+    # Create rotation matrix to transform normal to e_z
+    normal = normal / norm(normal)
+    c = normal + [0, 0, 1]
+    R = 2(c * c') / c'c - I(3)
+    normal ≈ [0, 0, 1] || (directions = R'directions)
+
+    directions
 end
